75663886f5a9072fef2ba7952891d5220fe28edd
John Doe <johndoe@example.com>
2024-08-21 17:53:34 -0500

    frist commit

---
+++ src/main/java/com/mafro/reserva_system/ReservaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mafro.reserva_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mafro/reserva_system/ReservaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafro.reserva_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
